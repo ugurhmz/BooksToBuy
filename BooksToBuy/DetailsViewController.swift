e2f0c2a2e6efd8ce4dd51ed8fee83ffea082622c
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-02-08 00:30:05 +0300

    NavigationBar, GestureRecognizer, all IBOutlet ,IBAction created

--- BooksToBuy/DetailsViewController.swift
+++ BooksToBuy/DetailsViewController.swift
@@ -1,20 +1,46 @@
-//
-//  DetailsViewController.swift
-//  BooksToBuy
-//
-//  Created by ugur-pc on 7.02.2022.
-//
+
 
 import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // fields
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var authorField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var pageField: UITextField!
+    @IBOutlet weak var publishedField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // hide keyboard when click in DetailsViewController
+        let gestureRecognizer = UITapGestureRecognizer(target: self,
+                                                       action:  #selector(hideKeyboard))
+        
+        // add Recog to VIEW
+        view.addGestureRecognizer(gestureRecognizer)
+        
         
     }
     
+    
+    // Hide Keyboard
+    @objc func hideKeyboard(){
+        view.endEditing(true) // Açık olan klavye kapansın.
+    }
+    
+    
 
+    // Save BTN
+    @IBAction func saveBtn(_ sender: Any) {
+        print("save clicked...")
+    }
+    
 
 }
--- BooksToBuy/ViewController.swift
+++ BooksToBuy/ViewController.swift
@@ -4,10 +4,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("SourceTree 3. commit")
+        
+        
+        // navigationBar
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add,
+            target: self,
+            action: #selector(navigateAddBtn))
     }
+    
+    
+    // Clicked navigate btn when to go detail
+    @objc func navigateAddBtn(){
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    
 
 
 }
